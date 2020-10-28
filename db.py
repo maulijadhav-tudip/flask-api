c0b569280f26d181be09765ab8e8669e2d43ce55
shivam.gupta <shivam.gupta@tudip.com>
2020-10-28 22:58:58 +0530

    Feature Update#1 Refactored the code - Refactored the main and db file code according to the mockup

--- db.py
+++ db.py
@@ -1,7 +1,8 @@
-#db.py
+"""Database interface for skills-db."""
+
 import os
 import pymysql
-from flask import jsonify
+FROM flask import jsonify
 
 db_user = os.environ.get('CLOUD_SQL_USERNAME')
 db_password = os.environ.get('CLOUD_SQL_PASSWORD')
@@ -10,23 +11,51 @@ db_connection_name = os.environ.get('CLOUD_SQL_CONNECTION_NAME')
 
 
 def open_connection():
+    """Open database connection."""
     unix_socket = '/cloudsql/{}'.format(db_connection_name)
     try:
         if os.environ.get('GAE_ENV') == 'standard':
-            conn = pymysql.connect(user=db_user, password=db_password,
-                                unix_socket=unix_socket, db=db_name,
-                                cursorclass=pymysql.cursors.DictCursor
-                                )
+            conn = pymysql.connect(
+                user=db_user,
+                password=db_password,
+                unix_socket=unix_socket,
+                db=db_name,
+                cursorclass=pymysql.cursors.DictCursor)
     except pymysql.MySQLError as e:
         print(e)
-
     return conn
 
 
-def get_skills(email):
+def check_login(email_id):
+    """Check user existence in database."""
     conn = open_connection()
     with conn.cursor() as cursor:
-        result = cursor.execute('select Users.user_id,skill_name,Taxonomy.skill_id,Taxonomy.skill_description_level_0,Taxonomy.skill_description_level_1,Taxonomy.skill_description_level_2,Taxonomy.skill_description_level_3,Taxonomy.skill_description_level_4 from UserSkillsPrioDB INNER JOIN Users on  UserSkillsPrioDB.user_id = Users.user_id inner join Taxonomy on UserSkillsPrioDB.skill_id = Taxonomy.skill_id where email_id = %s order by priority_rating;', email)
+        query = "SELECT * FROM Users WHERE email_id=%s;"
+        val = (email_id)
+        rst = cursor.execute(query, val)
+        if rst > 0:
+           return True
+        else:
+            name = email_id.split("@")[0]
+            role = 'CE'
+            organization = 'Google Cloud'
+            query1 = "INSERT INTO Users(email_id,name,role,organization) VALUES (%s ,%s,%s,%s);"
+            val = (email_id, name, role, organization)
+            result = cursor.execute(query1, val)
+            conn.commit()
+            query2 = "INSERT INTO UserSkillsPrioDB (user_id, skill_id) SELECT Users.user_id, Taxonomy.skill_id FROM Users INNER JOIN Taxonomy ON Users.role=Taxonomy.personality_type WHERE Users.email_id=%s"
+            val1 = (email_id)
+            result1 = cursor.execute(query2, val1)
+            conn.commit()
+            return True
+    conn.close()
+
+
+def get_skills(email):
+    """Retrieve skills from database."""
+    conn = open_connection()
+    with conn.cursor() as cursor:
+        result = cursor.execute('SELECT Taxonomy.skill_hierarchy_level_1,Taxonomy.skill_hierarchy_level_2,Taxonomy.skill_hierarchy_level_3, Users.user_id,skill_name,Taxonomy.skill_id,Taxonomy.skill_type,Taxonomy.skill_description_level_0,Taxonomy.skill_description_level_1,Taxonomy.skill_description_level_2,Taxonomy.skill_description_level_3,Taxonomy.skill_description_level_4, Taxonomy.skill_ranking_level_0, Taxonomy.skill_ranking_level_1, Taxonomy.skill_ranking_level_2, Taxonomy.skill_ranking_level_3, Taxonomy.skill_ranking_level_4 FROM UserSkillsPrioDB INNER JOIN Users ON  UserSkillsPrioDB.user_id = Users.user_id INNER JOIN Taxonomy ON UserSkillsPrioDB.skill_id = Taxonomy.skill_id WHERE email_id = %s order by priority_rating, skill_id,UserSkillsPrioDB.updated_at IS NULL DESC;', email)
         skills = cursor.fetchall()
         if result > 0:
             got_skills = jsonify(skills)
@@ -37,105 +66,138 @@ def get_skills(email):
 
 
 def save_rating(skill_id,user_id,rating):
+    """Persist rating to database."""
     conn = open_connection()
     with conn.cursor() as cursor:
-        query = "select * from UsersSkillDB where user_id=%s and skill_id=%s;"
-        val = (user_id,skill_id)
-        rst = cursor.execute(query,val)
-        if rst > 0:
-            sql = "update UsersSkillDB set skill_rating  =%s where user_id=%s and skill_id=%s;"
-            val = (rating,user_id,skill_id)
-
-        else:
-            sql = "insert into UsersSkillDB(user_id,skill_id,skill_rating) values ((select user_id from Users where user_id=%s),(select skill_id from Taxonomy where skill_id=%s),%s);"
-            val = (user_id,skill_id,rating)
-
-        result = cursor.execute(sql,val)
+        query = "INSERT INTO UsersSkillDB(user_id,skill_id,skill_rating) VALUES((SELECT user_id FROM Users WHERE user_id = %s),(SELECT skill_id FROM Taxonomy WHERE skill_id = %s),%s)"
+        val = (user_id, skill_id, rating)
+        result = cursor.execute(query, val)
         conn.commit()
     conn.close()
 
 
 def get_dashboard(email):
+    """Retrieve dashboard data from database."""
     conn = open_connection()
     with conn.cursor() as cursor:
-        result = cursor.execute('select Users.user_name,Taxonomy.skill_name,Taxonomy.skill_id,skill_rating from UsersSkillDB INNER JOIN Users on  UsersSkillDB.user_id = Users.user_id inner join Taxonomy on UsersSkillDB.skill_id = Taxonomy.skill_id where email_id = %s ;',email)
+        result = cursor.execute('SELECT Users.name,Taxonomy.skill_name,Taxonomy.skill_id,skill_rating FROM UsersSkillDB INNER JOIN Users ON  UsersSkillDB.user_id = Users.user_id INNER JOIN Taxonomy ON UsersSkillDB.skill_id = Taxonomy.skill_id WHERE email_id = %s ;',email)
         skills = cursor.fetchall()
         if result > 0:
-            got_skills = jsonify(skills)
+            got_dashboard = jsonify(skills)
         else:
-            got_skills = 'No self-attested data available in DB'
+            got_dashboard = 'No self-attested data available in DB'
     conn.close()
-    return got_skills
+    return got_dashboard
 
 
 def set_skill_priority(user_id, skill_id):
+    """Persist skill priority to database."""
     conn = open_connection()
     with conn.cursor() as cursor:
-        sql = "update UserSkillsPrioDB set priority_rating = ((select count(skill_id) from (select * from UserSkillsPrioDB where user_id = %s) as UserSkillsPrioDB_new1) + (select priority_rating from (select * from UserSkillsPrioDB where user_id = %s and skill_id = %s ) as UserSkillsPrioDB_new2)) where  user_id = %s and skill_id = %s ;"
-        val = (user_id, user_id, skill_id, user_id, skill_id)
-        result = cursor.execute(sql, val)
+        query = "UPDATE UserSkillsPrioDB SET priority_rating=30, updated_at=NOW() WHERE  user_id = %s AND skill_id = %s ;"
+        val = (user_id, skill_id)
+        result = cursor.execute(query, val)
         conn.commit()
     conn.close()
 
 
-def get_self_attest_count_per_day(user_id):
+def get_progress_count(user_id):
+    """Retrieve count of rated skills, unrated skill and expired skill from database."""
     conn = open_connection()
     with conn.cursor() as cursor:
-        result = cursor.execute('SELECT COUNT(skill_id) FROM UsersSkillDB where date(created_at)=current_date() and user_id = %s;', user_id)
-        skills = cursor.fetchall()
+        query = 'SELECT (SELECT COUNT(skill_id) FROM UserSkillsPrioDB WHERE updated_at IS NULL AND user_id = %s) AS unrated_skill_count,(SELECT COUNT(skill_id) FROM UserSkillsPrioDB WHERE updated_at IS NOT NULL AND priority_rating >= 1 AND user_id = %s) AS rated_skill_count,(SELECT COUNT(skill_id) FROM UserSkillsPrioDB WHERE updated_at IS NOT NULL AND priority_rating < 1 AND user_id = %s) AS expired_skill_count'
+        val = (user_id, user_id, user_id)
+        result = cursor.execute(query, val)
+        skills_count = cursor.fetchall()
         if result > 0:
-            got_skills = jsonify(skills)
+            got_skills_count = jsonify(skills_count)
         else:
-            got_skills = 'No self-attested data available in DB'
+            got_skills_count = 'No self-attested data available in DB'
     conn.close()
-    return got_skills
+    return got_skills_count
 
 
 def set_last_trigger_time(user_id, timestamp):
+    """Persist last trigger time to database."""
     conn = open_connection()
     with conn.cursor() as cursor:
-        rst = cursor.execute("select * from Survey where user_id=%s", user_id)
-        if rst > 0:
-            sql = "update Survey set trigger_count  = ((select trigger_count from (select * from Survey where user_id = %s) as Survey_new) + 1), ext_triggered = %s where  user_id = %s;"
-            val = (user_id, timestamp, user_id)
+        query = "INSERT INTO Survey(user_id,ext_triggered, ext_closed, ext_postponed, postpone_option) VALUES ((SELECT user_id FROM Users WHERE user_id = %s),%s,null,null,null);"
+        val = (user_id, timestamp)
 
-        else:
-            sql = "insert into Survey values (%s,%s,null,null,1);"
-            val = (user_id, timestamp)
-
-        result = cursor.execute(sql,val)
+        result = cursor.execute(query, val)
         conn.commit()
     conn.close()
 
 
 def log_extension_closed_time(user_id, timestamp):
+    """Persist extension closed time to database."""
     conn = open_connection()
     with conn.cursor() as cursor:
-        sql = "update Survey set ext_closed = %s where  user_id = %s;"
-        val = (timestamp, user_id)
-        result = cursor.execute(sql,val)
-        conn.commit()
-    conn.close()
-
-
-def log_extension_postponed_time(user_id, timestamp):
-    conn = open_connection()
-    with conn.cursor() as cursor:
-        sql = "update Survey set ext_postponed = %s where  user_id = %s;"
-        val = (timestamp, user_id)
+        sql = "UPDATE Survey SET ext_closed = %s WHERE user_id = (SELECT user_id FROM (SELECT * FROM Survey WHERE user_id = %s ORDER BY ext_triggered DESC limit 1) as Survey_new) AND ext_triggered = (SELECT ext_triggered FROM (SELECT * FROM Survey WHERE user_id = %s ORDER BY ext_triggered DESC limit 1) as Survey_new);"
+        val = (timestamp, user_id, user_id)
         result = cursor.execute(sql, val)
         conn.commit()
     conn.close()
 
 
-def get_survey_data(user_id):
+def log_extension_postponed_time(user_id, timestamp, postpone_option):
+    """Persist extension postponed time to database."""
     conn = open_connection()
     with conn.cursor() as cursor:
-        result = cursor.execute('select * from Survey where user_id = %s ;', user_id)
+        sql = "UPDATE Survey SET ext_postponed = %s ,postpone_option = %s WHERE user_id = (SELECT user_id FROM (SELECT * FROM Survey WHERE user_id = %s ORDER BY ext_triggered DESC limit 1) as Survey_new) AND ext_triggered = (SELECT ext_triggered FROM (SELECT * FROM Survey WHERE user_id = %s ORDER BY ext_triggered DESC limit 1) as Survey_new);"
+        val = (timestamp, postpone_option, user_id, user_id)
+        result = cursor.execute(sql, val)
+        conn.commit()
+    conn.close()
+
+
+def get_survey_data(email):
+    """Retrieve survey data from database."""
+    conn = open_connection()
+    with conn.cursor() as cursor:
+        query = 'SELECT Users.user_id, Survey.ext_triggered, Survey.ext_closed, Survey.ext_postponed FROM Survey INNER JOIN Users ON Survey.user_id = Users.user_id WHERE email_id = %s ORDER BY ext_triggered DESC LIMIT 1;'
+        result = cursor.execute(query, email)
         skills = cursor.fetchall()
         if result > 0:
-            got_skills = jsonify(skills)
+            survey_data = jsonify(skills)
         else:
-            got_skills = 'No self-attested data available in DB'
+            survey_data = 'No skills in DB'
     conn.close()
-    return got_skills
+    return survey_data
+
+
+def update_prompting():
+    """Update priority rating in database."""
+    conn = open_connection()
+    with conn.cursor() as cursor:
+        query = "UPDATE UserSkillsPrioDB SET priority_rating=30 - DATEDIFF(now(), updated_at)  WHERE updated_at IS NOT NULL;"
+        result = cursor.execute(query)
+        conn.commit()
+    conn.close()
+
+
+def get_triggered_count_per_day(email):
+    """Get number of times the popup is triggered."""
+    conn = open_connection()
+    with conn.cursor() as cursor:
+        query = 'SELECT COUNT(Survey.user_id) AS trigger_count FROM Survey INNER JOIN Users ON Users.user_id = Survey.user_id WHERE Users.email_id = %s;'
+        result = cursor.execute(query, email)
+        count = cursor.fetchall()
+        trigger_count = jsonify(count)
+    conn.close()
+    return trigger_count
+
+
+def delete_user_data(user_id):
+    """Delete user related data from database."""
+    conn = open_connection()
+    with conn.cursor() as cursor:
+        query1 = 'DELETE UsersSkillDB,Survey FROM UsersSkillDB INNER JOIN Survey ON UsersSkillDB.user_id=Survey.user_id WHERE Survey.user_id=%s'
+        result1 = cursor.execute(query1, user_id)
+        query2 = 'UPDATE UserSkillsPrioDB SET priority_rating=NULL, updated_at=NULL WHERE  user_id = %s'
+        result2 = cursor.execute(query2, user_id)
+        conn.commit()
+    conn.close()
+    return 'Data deleted successfully'
+
+
--- main.py
+++ main.py
@@ -1,22 +1,38 @@
-from flask import Flask, jsonify, request
-from db import get_skills, save_rating, get_dashboard, set_skill_priority, get_self_attest_count_per_day, set_last_trigger_time, log_extension_closed_time, log_extension_postponed_time, get_survey_data
+"""Main file for Skills-db backend."""
+
+from db import get_dashboard
+from db import get_progress_count
+from db import get_skills
+from db import get_survey_data
+from db import log_extension_closed_time
+from db import log_extension_postponed_time
+from db import save_rating
+from db import set_last_trigger_time
+from db import set_skill_priority
+from db import update_prompting
+from db import check_login
+from flask import request
+from flask import Flask
+from flask import jsonify
 
 app = Flask(__name__)
 
 
 @app.route('/get-skills', methods=['GET'])
 def skills():
-
+    """Endpoint to check user information in database."""
     if request.args:
         email = request.args.get('email')
+        check_login(email)
         return get_skills(email)
     else:
-        got_skills = 'Invalid parameters'
-        return got_skills
+        error_msg = 'Invalid parameters'
+        return error_msg
 
 
 @app.route('/rate-skill', methods=['POST'])
 def rate_skill():
+    """Endpoint to persist skill rating to database."""
     if request.method == 'POST':
         if request.form:
             skill_id = request.form.get("skillId")
@@ -35,16 +51,18 @@ def rate_skill():
 
 @app.route('/get-dashboard', methods=['GET'])
 def dashboard():
+    """Endpoint to retrieve dashboard data from database."""
     if request.args:
         email = request.args.get('email')
         return get_dashboard(email)
     else:
-        got_skills = 'Invalid parameters'
-        return got_skills
+        error_msg = 'Invalid parameters'
+        return error_msg
 
 
 @app.route('/update-skill-priority', methods=['POST'])
 def update_skill_priority():
+    """Endpoint to update skill priority in database."""
     if request.method == 'POST':
         if request.form:
             skill_id = request.form.get("skillId")
@@ -60,18 +78,20 @@ def update_skill_priority():
         return error_msg
 
 
-@app.route('/get-self-attest-count-per-day', methods=['GET'])
-def self_attest_count():
+@app.route('/get-progress-count', methods=['GET'])
+def progress_skill_count():
+    """Endpoint to retrieve count of rated skill, unrated skill and expired skill from database."""
     if request.args:
         user_id = request.args.get('userId')
-        return get_self_attest_count_per_day(user_id)
+        skills_count = get_progress_count(user_id)
     else:
-        got_skills = 'Invalid parameters'
-        return got_skills
+        skills_count = 'Invalid parameters'
+    return skills_count
 
 
 @app.route('/save-trigger-time', methods=['POST'])
 def save_trigger_time_and_count():
+    """Endpoint to persist trigger time and count to database."""
     if request.method == 'POST':
         if request.form:
             timestamp = request.form.get("timestamp")
@@ -85,8 +105,10 @@ def save_trigger_time_and_count():
         error_msg = 'Invalid method'
         return error_msg
 
+
 @app.route('/log-extension-closed-time', methods=['POST'])
 def save_closed_time():
+    """Endpoint to persist closed time to database."""
     if request.method == 'POST':
         if request.form:
             timestamp = request.form.get("timestamp")
@@ -100,14 +122,17 @@ def save_closed_time():
         error_msg = 'Invalid method'
         return error_msg
 
+
 @app.route('/log-extension-postponed-time', methods=['POST'])
 def save_postponed_time():
+    """Endpoint to persist postponed time to database."""
     if request.method == 'POST':
         if request.form:
             timestamp = request.form.get("timestamp")
             user_id = request.form.get("userId")
+            postpone_option = request.form.get("postponeOption")
 
-            log_extension_postponed_time(user_id, timestamp)
+            log_extension_postponed_time(user_id, timestamp, postpone_option)
             return 'Extension postpone event logged successfully.'
         else:
             return jsonify({"msg": "Missing form data in request"}), 400
@@ -118,14 +143,43 @@ def save_postponed_time():
 
 @app.route('/get-survey-data', methods=['GET'])
 def survey_data():
+    """Endpoint to retrieve survey data from database."""
+    if request.args:
+        email = request.args.get('email')
+        return get_survey_data(email)
+    else:
+        error_msg = 'Invalid parameters'
+        return error_msg
+
+
+@app.route('/triggered-count-per-day', methods=['GET'])
+def triggered_count():
+    """Endpoint to retrieve triggered count from database."""
+    if request.args:
+        email = request.args.get('email')
+        return get_triggered_count_per_day(email)
+    else:
+        error_msg = 'Invalid parameters'
+        return error_msg
+
+
+@app.route('/set-skill-priority', methods=['GET'])
+def update_skill_priority_per_day():
+    """Endpoint to update skill priority."""
+    return update_prompting()
+
+
+@app.route('/delete-user-data', methods=['GET'])
+def delete_history():
+    """Endpoint to delete user related data from database."""
     if request.args:
         user_id = request.args.get('userId')
-        return get_survey_data(user_id)
+        return delete_user_data(user_id)
     else:
-        got_skills = 'Invalid parameters'
-        return got_skills
-
+        error_msg = 'Invalid parameters'
+        return error_msg
 
 
 if __name__ == '__main__':
     app.run()
+
